8aab970e02cb7acc791c69037d764b907febb2b7
Alexr16 <j.alex-1996@hotmail.com>
2022-02-28 16:16:14 -0500

    fix linter errors

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const hamburguer = document.querySelector(".menu");
-const navMenu = document.querySelector(".list");
-const icons = document.querySelectorAll(".menu-icon");
-const hamburguerIcon = document.querySelector("#hamburguer")
+const hamburguer = document.querySelector('.menu');
+const navMenu = document.querySelector('.list');
+const icons = document.querySelectorAll('.menu-icon');
+const hamburguerIcon = document.querySelector('#hamburguer')
 
-hamburguer.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-    icons.forEach(icon => {
-        icon.classList.toggle("close-menu");
-    });
-})
+hamburguer.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+  icons.forEach(icon => {
+    icon.classList.toggle('close-menu');
+  });
+});
 
-document.querySelectorAll(".item").forEach(n => n.addEventListener("click", function(){
-    icons.forEach(icon => {
-        icon.classList.add("close-menu")
-    })
-    hamburguerIcon.classList.remove("close-menu")
-    navMenu.classList.toggle("active");
- }))
+document.querySelectorAll('.item').forEach(n => n.addEventListener('click', function(){
+  icons.forEach(icon => {
+    icon.classList.add('close-menu');
+  })
+  hamburguerIcon.classList.remove('close-menu');
+  navMenu.classList.toggle('active');
+}));
  
