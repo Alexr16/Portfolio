05ca3de1c380fc94e892ae2d89fc8bace42a4c1c
Alexr16 <j.alex-1996@hotmail.com>
2022-02-28 16:20:04 -0500

    fix linter errors

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const hamburguer = document.querySelector('.menu');
 const navMenu = document.querySelector('.list');
 const icons = document.querySelectorAll('.menu-icon');
-const hamburguerIcon = document.querySelector('#hamburguer')
+const hamburguerIcon = document.querySelector('#hamburguer');
 
 hamburguer.addEventListener('click', () => {
   navMenu.classList.toggle('active');
@@ -13,8 +13,7 @@ hamburguer.addEventListener('click', () => {
 document.querySelectorAll('.item').forEach(n => n.addEventListener('click', function(){
   icons.forEach(icon => {
     icon.classList.add('close-menu');
-  })
+  });
   hamburguerIcon.classList.remove('close-menu');
   navMenu.classList.toggle('active');
 }));
- 
